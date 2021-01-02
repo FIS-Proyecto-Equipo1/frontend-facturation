b0fed3a7b5dd3e1f3705d0eced03cd2a9b13377a
Julia Palacios <moreparaya@gmail.com>
2021-01-02 12:12:58 +0100

    added dropdown to chose status and other modifications

---
+++ src/BillsApi.js
@@ -0,0 +1,64 @@
+class BillsApi {
+    //static API_BASE_URL = "https://fis-2020-bills.herokuapp.com/api/v1";
+    static API_BASE_URL = "http://localhost:3000/api/v1";
+
+    static requestHeaders(){
+        return {}
+    }
+
+    static getAllVehicles(){
+        const headers = this.requestHeaders();
+        const request = new Request(VehiculosApi.API_BASE_URL + "/bills", {
+            method: 'GET',
+            headers: headers
+        });
+
+        return fetch(request).then(response => {
+            return response.json();
+        })
+    }
+    
+    static postBill(bill){
+        const headers = {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        };
+        return fetch(BillsApi.API_BASE_URL + "/bills", {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify({
+                billNumber: bill.billNumber,
+                name: bill.name,
+                surnames: bill.surnames,
+                vehicle: bill.vehicle,
+                amount: bill.amount,
+                status: bill.status    
+            })}).then(response => {
+            console.log(response);
+            return response.json();
+        });
+    }
+    static updateBill(bill){
+        const headers = {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        };
+        console.log(bill);
+        return fetch(BillsApi.API_BASE_URL + "/bills/"+bill.billNumber, {
+            method: 'PUT',
+            headers: headers,
+            body: JSON.stringify({
+                billNumber: bill.billNumber,
+                name: bill.name,
+                surnames: bill.surnames,
+                vehicle: bill.vehicle,
+                amount: bill.amount,
+                status: bill.status   
+        })}).then(response => {
+            console.log(response);
+            return response.json();
+        });
+    }
+}
+
+export default BillsApi;
--- src/EditBill.js
+++ src/EditBill.js
@@ -7,12 +7,18 @@ function EditBill(props) {
 
     return (
         <tr>
-            <td><input className="form-control" name="billNumber" value={props.bill.billNumber} onChange={handleChange}/></td>
-            <td><input className="form-control" name="name" value={props.bill.name} onChange={handleChange}/></td>
-            <td><input className="form-control" name="surnames" value={props.bill.surnames} onChange={handleChange}/></td>
-            <td><input className="form-control" name="vehicle" value={props.bill.vehicle} onChange={handleChange}/></td>
-            <td><input className="form-control" name="amount" value={props.bill.amount} onChange={handleChange}/></td>
-            <td><input className="form-control" name="billStatus" value={props.bill.billStatus} onChange={handleChange}/></td>
+        
+            <td><input className="form-control" name="billNumber" value={props.bill.billNumber}/></td>
+            <td><input className="form-control" name="name" value={props.bill.name}/></td>
+            <td><input className="form-control" name="surnames" value={props.bill.surnames}/></td>
+            <td><input className="form-control" name="vehicle" value={props.bill.vehicle}/></td>
+            <td><input className="form-control" name="amount" value={props.bill.amount}/></td>
+            <td>
+            <select className="form-control" name="billStatus" value={props.bill.billStatus} onChange={handleChange}>
+                <option value="PAID">PAID</option> 
+                <option value="UNPAID">UNPAID</option> 
+            </select>
+            </td>
             <td>
                 <button className="btn btn-primary" onClick={() => props.onSave(props.bill)}>Save</button>
                 <button className="btn btn-primary" onClick={() => props.onCancel(props.bill)}>Cancel</button>
